8256aef968207bea1459daec529f0cf615b6efed
Vishwash <vishwash.iitrpr@gmail.com>
2025-12-05 17:15:56 +0000

    Update settings.py

--- progress_tracker/progress_tracker/settings.py
+++ progress_tracker/progress_tracker/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#import environ
-#env = environ.Env()
-#environ.Env.read_env()
-SECRET_KEY = "django-insecure-@gjlx9gwf=pj6464i*7anr!52nkot86n$4-^99xf1ip%awjr+0"
+import environ
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
