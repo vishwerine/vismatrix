8f951a8dec8c9601dc338655e93e3ba96bec3fe1
Vishwash <vishwash.iitrpr@gmail.com>
2025-12-06 02:06:42 +0000

    Update settings.py

--- progress_tracker/progress_tracker/settings.py
+++ progress_tracker/progress_tracker/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['vismatrix.space','www.vismatrix.space','16.16.9.54','localhost']
