942226f49f66ddd58c80786fba8e09a48c12a141
Vishwash Batra <vishwashbatra@192.168.1.104>
2026-01-14 17:58:03 +0000

    icon fix

--- progress_tracker/progress_tracker/urls.py
+++ progress_tracker/progress_tracker/urls.py
@@ -17,10 +17,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Allauth URLs
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'branding/vismatrixicon.png', permanent=True)),
     path('', include('tracker.urls')),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
